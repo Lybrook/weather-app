fb0944533bcdb33d68da778aeaa258b0991ccabd
Lybrook Omondi <omoshlybrook@gmail.com>
2025-05-14 02:11:31 +0300

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,5 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
 
-Route::get('/weather/city', [WeatherController::class, 'getWeatherByCity']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Weather API endpoints
+Route::get('/weather', [WeatherController::class, 'getWeatherByCity']);
 Route::get('/weather/coordinates', [WeatherController::class, 'getWeatherByCoordinates']);
